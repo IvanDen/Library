8cbf75b653d33891618cc101ccc2ae8b14667626
Ivan <48669597+IvanDen@users.noreply.github.com>
2020-11-01 19:55:27 +0300

    Add project files.

---
+++ LibraryDate/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryDate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryDate
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
---
+++ LibraryDate/Models/Patron.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryDate.Models
+{
+    public class Patron
+    {
+        #region properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+        #endregion
+
+        //public virtual LibraryCard LibraryCard { get; set; }
+    }
+}
